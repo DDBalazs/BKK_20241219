ffd607a7f5bda8bd585a58c6a3dbb40cf909a286
Balázs Zsolt Csőke <csoke.balazs@verebelyszki.hu>
2024-12-19 11:31:09 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BKK_20241219
+{
+    partial class frmfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.buszjegyÉsBérletToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kilépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vásárlástToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statisztikaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buszjegyÉsBérletToolStripMenuItem,
+            this.kilépésToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(933, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // buszjegyÉsBérletToolStripMenuItem
+            // 
+            this.buszjegyÉsBérletToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vásárlástToolStripMenuItem,
+            this.statisztikaToolStripMenuItem});
+            this.buszjegyÉsBérletToolStripMenuItem.Name = "buszjegyÉsBérletToolStripMenuItem";
+            this.buszjegyÉsBérletToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.buszjegyÉsBérletToolStripMenuItem.Text = "Buszjegy és bérlet";
+            // 
+            // kilépésToolStripMenuItem
+            // 
+            this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
+            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.kilépésToolStripMenuItem.Text = "Kilépés";
+            this.kilépésToolStripMenuItem.Click += new System.EventHandler(this.kilépésToolStripMenuItem_Click);
+            // 
+            // vásárlástToolStripMenuItem
+            // 
+            this.vásárlástToolStripMenuItem.Name = "vásárlástToolStripMenuItem";
+            this.vásárlástToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vásárlástToolStripMenuItem.Text = "Vásárlás";
+            // 
+            // statisztikaToolStripMenuItem
+            // 
+            this.statisztikaToolStripMenuItem.Name = "statisztikaToolStripMenuItem";
+            this.statisztikaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.statisztikaToolStripMenuItem.Text = "Statisztika";
+            // 
+            // frmfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Főform";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem buszjegyÉsBérletToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vásárlástToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem statisztikaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kilépésToolStripMenuItem;
+    }
+}
+
---
+++ frmvasarlas.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BKK_20241219
+{
+    partial class frmvasarlas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txvondb = new System.Windows.Forms.TextBox();
+            this.txvonteljdb = new System.Windows.Forms.TextBox();
+            this.txdiakdb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbfizetendo = new System.Windows.Forms.Label();
+            this.lbvonossz = new System.Windows.Forms.Label();
+            this.lbteljossz = new System.Windows.Forms.Label();
+            this.lbdiakossz = new System.Windows.Forms.Label();
+            this.btfizetes = new System.Windows.Forms.Button();
+            this.btelvet = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vonaljegy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Teljes árú bérlet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Diák bérlet";
+            // 
+            // txvondb
+            // 
+            this.txvondb.Location = new System.Drawing.Point(112, 29);
+            this.txvondb.Name = "txvondb";
+            this.txvondb.Size = new System.Drawing.Size(100, 20);
+            this.txvondb.TabIndex = 3;
+            // 
+            // txvonteljdb
+            // 
+            this.txvonteljdb.Location = new System.Drawing.Point(112, 75);
+            this.txvonteljdb.Name = "txvonteljdb";
+            this.txvonteljdb.Size = new System.Drawing.Size(100, 20);
+            this.txvonteljdb.TabIndex = 4;
+            // 
+            // txdiakdb
+            // 
+            this.txdiakdb.Location = new System.Drawing.Point(112, 122);
+            this.txdiakdb.Name = "txdiakdb";
+            this.txdiakdb.Size = new System.Drawing.Size(100, 20);
+            this.txdiakdb.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "db";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(231, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "db";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(231, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "db";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbdiakossz);
+            this.panel1.Controls.Add(this.lbteljossz);
+            this.panel1.Controls.Add(this.lbvonossz);
+            this.panel1.Controls.Add(this.lbfizetendo);
+            this.panel1.Location = new System.Drawing.Point(15, 174);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(237, 153);
+            this.panel1.TabIndex = 9;
+            // 
+            // lbfizetendo
+            // 
+            this.lbfizetendo.AutoSize = true;
+            this.lbfizetendo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbfizetendo.ForeColor = System.Drawing.Color.Red;
+            this.lbfizetendo.Location = new System.Drawing.Point(15, 16);
+            this.lbfizetendo.Name = "lbfizetendo";
+            this.lbfizetendo.Size = new System.Drawing.Size(52, 17);
+            this.lbfizetendo.TabIndex = 10;
+            this.lbfizetendo.Text = "label7";
+            // 
+            // lbvonossz
+            // 
+            this.lbvonossz.AutoSize = true;
+            this.lbvonossz.Location = new System.Drawing.Point(15, 58);
+            this.lbvonossz.Name = "lbvonossz";
+            this.lbvonossz.Size = new System.Drawing.Size(41, 13);
+            this.lbvonossz.TabIndex = 11;
+            this.lbvonossz.Text = "label8";
+            // 
+            // lbteljossz
+            // 
+            this.lbteljossz.AutoSize = true;
+            this.lbteljossz.Location = new System.Drawing.Point(15, 92);
+            this.lbteljossz.Name = "lbteljossz";
+            this.lbteljossz.Size = new System.Drawing.Size(41, 13);
+            this.lbteljossz.TabIndex = 12;
+            this.lbteljossz.Text = "label9";
+            // 
+            // lbdiakossz
+            // 
+            this.lbdiakossz.AutoSize = true;
+            this.lbdiakossz.Location = new System.Drawing.Point(15, 127);
+            this.lbdiakossz.Name = "lbdiakossz";
+            this.lbdiakossz.Size = new System.Drawing.Size(48, 13);
+            this.lbdiakossz.TabIndex = 13;
+            this.lbdiakossz.Text = "label10";
+            // 
+            // btfizetes
+            // 
+            this.btfizetes.Image = global::BKK_20241219.Properties.Resources.oke;
+            this.btfizetes.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btfizetes.Location = new System.Drawing.Point(15, 353);
+            this.btfizetes.Name = "btfizetes";
+            this.btfizetes.Size = new System.Drawing.Size(115, 59);
+            this.btfizetes.TabIndex = 10;
+            this.btfizetes.Text = "Fizetés";
+            this.btfizetes.UseVisualStyleBackColor = true;
+            // 
+            // btelvet
+            // 
+            this.btelvet.Image = global::BKK_20241219.Properties.Resources.elvet;
+            this.btelvet.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btelvet.Location = new System.Drawing.Point(145, 353);
+            this.btelvet.Name = "btelvet";
+            this.btelvet.Size = new System.Drawing.Size(107, 59);
+            this.btelvet.TabIndex = 11;
+            this.btelvet.Text = "Elvet";
+            this.btelvet.UseVisualStyleBackColor = true;
+            // 
+            // frmvasarlas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 450);
+            this.Controls.Add(this.btelvet);
+            this.Controls.Add(this.btfizetes);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txdiakdb);
+            this.Controls.Add(this.txvonteljdb);
+            this.Controls.Add(this.txvondb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmvasarlas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jegy és bérlet vásárlás";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txvondb;
+        private System.Windows.Forms.TextBox txvonteljdb;
+        private System.Windows.Forms.TextBox txdiakdb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbdiakossz;
+        private System.Windows.Forms.Label lbteljossz;
+        private System.Windows.Forms.Label lbvonossz;
+        private System.Windows.Forms.Label lbfizetendo;
+        private System.Windows.Forms.Button btfizetes;
+        private System.Windows.Forms.Button btelvet;
+    }
+}
